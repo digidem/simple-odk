52bf95df64ab35a9464255c5c70a52c281b3489e
Gregor MacLennan <gmaclennan@digital-democracy.org>
2014-10-20 18:53:56 -0700

    Ignore unauthorized errors in New Relic

--- newrelic.js
+++ newrelic.js
@@ -13,6 +13,20 @@ exports.config = {
    * Your New Relic license key.
    */
   license_key : process.env.NEWRELIC_KEY,
+  error_collector : {
+    enabled : true,
+    /**
+     * List of HTTP error status codes the error tracer should disregard.
+     * Ignoring a status code means that the transaction is not renamed to
+     * match the code, and the request is not treated as an error by the error
+     * collector.
+     *
+     * Defaults to 404 NOT FOUND.
+     *
+     * @env NEW_RELIC_ERROR_COLLECTOR_IGNORE_ERROR_CODES
+     */
+    ignore_status_codes : [404, 401]
+  },
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
