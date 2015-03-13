1f9fd35f41cd4ae88dd6e40dc1034c22101a00ce
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-03-13 13:42:38 -0500

    Add test

---
+++ tests/helpers/update-file-ref.test.js
@@ -0,0 +1,16 @@
+var test = require('tape');
+var updateFileRef = require('../../helpers/update-file-ref');
+
+var form = require('../fixtures/form-media');
+
+var file = {
+  originalFilename: '123456789.jpg',
+  url: 'http://example.com/images/123456789.jpg'
+};
+
+test('Updates form replacing the filename with url and original filename', function(t) {
+  var expected = require('../fixtures/form-media-updated');
+  updateFileRef(form, file);
+  t.deepEqual(form, expected, 'form matches expected');
+  t.end();
+});
