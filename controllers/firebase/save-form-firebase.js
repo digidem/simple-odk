70b85a6720cd8da390c4dcc657e08f26250bb8b0
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-03-28 17:10:19 -0700

    Add firebase performance timing

--- controllers/firebase/save-form-firebase.js
+++ controllers/firebase/save-form-firebase.js
@@ -5,13 +5,14 @@ var debug = require('debug')('simple-odk:save-form-firebase')
 function saveForm (req, res, next) {
   var submission = req.submission
   var ref = req.firebase
+  var t0 = Date.now()
 
   ref.child('submissions')
     .child(submission.formId)
     .child(submission.instanceId)
     .set(submission.json, function (err) {
       if (err) return next(err)
-      debug('saved form response %s to firebase', submission.instanceId)
+      debug('saved form %s to firebase in %s ms', submission.instanceId, Date.now() - t0)
       res.status(201).send({
         saved: submission.instanceId
       })
