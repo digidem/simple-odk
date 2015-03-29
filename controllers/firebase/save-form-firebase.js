37f89ef1c0995d9faab4f14246946d5f439af647
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-03-29 15:42:58 -0700

    Rewrite Firebase integration
    Use cache-manager and use REST API instead of javascript client

--- controllers/firebase/save-form-firebase.js
+++ controllers/firebase/save-form-firebase.js
@@ -1,22 +1,28 @@
-// Saves a submission to firebase
-
+var request = require('request')
 var debug = require('debug')('simple-odk:save-form-firebase')
 
+// Saves a submission to firebase
 function saveForm (req, res, next) {
   var submission = req.submission
-  var ref = req.firebase
   var t0 = Date.now()
 
-  ref.child('submissions')
-    .child(submission.formId)
-    .child(submission.instanceId)
-    .set(submission.json, function (err) {
-      if (err) return next(err)
-      res.status(201).send({
-        saved: submission.instanceId
-      })
-      debug('saved form %s to firebase in %s ms', submission.instanceId, Date.now() - t0)
+  var url = 'https://' + req.params.appname + '.firebaseio.com/submissions/' +
+    submission.formId + '/' +
+    submission.instanceId + '?auth=' +
+    req.firebase_token
+
+  request({
+    url: url,
+    method: 'POST',
+    body: submission.json,
+    json: true
+  }, function (err, response, body) {
+    if (err) return next(err)
+    res.status(201).send({
+      saved: submission.instanceId
     })
+    debug('saved form %s to firebase in %s ms', submission.instanceId, Date.now() - t0)
+  })
 }
 
 module.exports = saveForm
--- middlewares/firebase-auth.js
+++ middlewares/firebase-auth.js
@@ -1,10 +1,10 @@
-var auth = require('basic-auth')
-var Firebase = require('firebase')
+var basicAuth = require('basic-auth')
+var request = require('request')
 var debug = require('debug')('simple-odk:fb-auth')
-var NodeCache = require('node-cache')
+var cacheManager = require('cache-manager')
 var createHash = require('../helpers/sha-hash.js')
 
-var authCache = new NodeCache({ stdTTL: 300 })
+var authCache = cacheManager.caching({store: 'memory', max: 500, ttl: 300/*seconds*/})
 
 /**
  * Middleware to authenticate to Firebase with Basic Auth. Attaches the
@@ -12,44 +12,36 @@ var authCache = new NodeCache({ stdTTL: 300 })
  */
 function FirebaseAuth () {
   return function (req, res, next) {
-    var user = auth(req)
+    var auth = basicAuth(req)
     var firebaseApp = req.params.appname
     var t0 = Date.now()
 
-    if (user === undefined || !firebaseApp) return unauthorized()
+    if (auth === undefined || !firebaseApp) return unauthorized()
 
     debug('checking firebase auth')
 
-    // We're going to cache auth details for 5 mins, so we avoid
-    // repeat lookups on Firebase, but we'll only cache a hash of
-    // the auth header, for a little added security
-    var hash = createHash(req.headers.authorization)
+    // We're going to cache the firebase Auth token for this user/password combo
+    var cacheKey = 'fb_auth' + firebaseApp + createHash(req.headers.authorization)
+    console.log(cacheKey)
 
     // Check if we have already checked this user/pass
-    // and attach the Firebase object to the request
-    // this just caches our check which initially forces ODK collect
-    // to send and Authorization header
-    authCache.get(hash, function (err, value) {
-      if (!err && value[hash]) {
-        req.firebase = value[hash]
-        debug('user auth cached, authorized in %s ms', Date.now() - t0)
-        return next()
-      }
-
-      var ref = new Firebase('https://' + firebaseApp + '.firebaseio.com')
-
-      ref.authWithPassword({
-        email: user.name,
-        password: user.pass
-      }, authHandler)
-
-      function authHandler (err) {
-        if (err) return unauthorized()
-        // Cache the authorization in memory for 5 mins
-        authCache.set(hash, ref)
-        req.firebase = ref
-        debug('authorized user in %s ms', Date.now() - t0)
+    // and attach the Firebase auth token to the request
+    authCache.wrap(cacheKey, function (cb) {
+      debug('authorizing against firebase app', firebaseApp)
+      getAuthToken({
+        name: auth.name,
+        pass: auth.pass,
+        appname: firebaseApp
+      }, cb)
+    }, function (err, token) {
+      if (!err) {
+        req.firebase_token = token
+        debug('user authorized in %s ms', Date.now() - t0)
         next()
+      } else if (err.statusCode === 401) {
+        unauthorized()
+      } else {
+        next(err)
       }
     })
 
@@ -59,6 +51,31 @@ function FirebaseAuth () {
       res.send('Unauthorized')
     }
   }
+}
+
+function getAuthToken (options, callback) {
+  var authUrl = 'https://auth.firebase.com/v2/' + options.appname +
+    '/auth/password?email=' + options.name +
+    '&password=' + options.pass +
+    '&transport=json'
+  request.get(authUrl, function (err, response, body) {
+      if (response.statusCode === 200) {
+        console.log(err)
+        try {
+          var token = JSON.parse(body).token
+          console.log(token)
+          callback(null, token)
+        } catch (e) {
+          console.log(e)
+          callback(e)
+        }
+      } else {
+        console.log(err)
+        err = err || new Error('Authentication error')
+        err.status = response.statusCode
+        callback(err)
+      }
+    })
 }
 
 module.exports = FirebaseAuth
