2f3ee33a1b186792c5a2d539e4d2eec037c1ad24
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-03-29 13:48:11 -0700

    Rewrite Github auth cache to use cache-manager

--- middlewares/github-auth-passthrough.js
+++ middlewares/github-auth-passthrough.js
@@ -1,10 +1,10 @@
 var basicAuth = require('basic-auth')
 var request = require('request')
 var debug = require('debug')('simple-odk:github-auth')
-var NodeCache = require('node-cache')
+var cacheManager = require('cache-manager')
 var createHash = require('../helpers/sha-hash.js')
 
-var authCache = new NodeCache({ stdTTL: 300 })
+var authCache = cacheManager.caching({store: 'memory', max: 500, ttl: 300/*seconds*/})
 
 request = request.defaults({
   headers: { 'User-Agent': 'simple-odk' }
@@ -20,6 +20,7 @@ request = request.defaults({
 function GithubAuth () {
   return function (req, res, next) {
     var auth = basicAuth(req)
+    var t0 = Date.now()
 
     if (auth === undefined) return unauthorized()
 
@@ -28,37 +29,22 @@ function GithubAuth () {
     // We're going to cache auth details for 5 mins, so we avoid
     // repeat lookups on Github, but we'll only cache a hash of
     // the auth header, for a little added security
-    var hash = createHash(req.headers.authorization)
+    var cacheKey = 'gh_auth' + createHash(req.headers.authorization)
 
     // Check if we have already checked this user/pass
     // Authorization will always be handled by the Github API,
     // this just caches our check which initially forces ODK collect
-    // to send and Authorization header
-    authCache.get(hash, function (err, value) {
-      if (!err & value[hash]) {
-        debug('user auth cached, authorized')
-        return next()
-      }
-
+    // to send an Authorization header
+    authCache.wrap(cacheKey, function (cb) {
       debug('authorizing against api.github.com/user')
-      request
-        .get('https://api.github.com/user')
-        .auth(auth.name, auth.pass, true)
-        .on('error', next)
-        .on('response', function (response) {
-          var unauthorized = (response.statusCode === 401 || response.statusCode === 404)
-          if (unauthorized) return unauthorized()
-          if (response.statusCode === 200) return authorized(hash)
-          var err = new Error('Authentication error')
-          err.status = response.statusCode
-          next(err)
-        })
+      checkAuth(auth, cb)
+    }, function (err, isAuthenticated) {
+      if (err) return next(err)
+      isAuthenticated ? authorized() : unauthorized()
     })
 
-    function authorized (hash) {
-      // Cache the authorization in memory for 5 mins
-      authCache.set(hash, true)
-      debug('user authorized')
+    function authorized () {
+      debug('user authorized in %s ms', Date.now() - t0)
       next()
     }
 
@@ -68,6 +54,21 @@ function GithubAuth () {
       res.send('Unauthorized')
     }
   }
+}
+
+function checkAuth (auth, callback) {
+  request
+    .get('https://api.github.com/user')
+    .auth(auth.name, auth.pass, true)
+    .on('error', callback)
+    .on('response', function (response) {
+      var authfail = (response.statusCode === 401 || response.statusCode === 404)
+      if (authfail) return callback(null, false)
+      if (response.statusCode === 200) return callback(null, true)
+      var err = new Error('Authentication error')
+      err.status = response.statusCode
+      callback(err)
+    })
 }
 
 module.exports = GithubAuth
