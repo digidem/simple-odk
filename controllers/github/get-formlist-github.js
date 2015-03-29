f3a31736a59de91367563d39f36178deb2052b63
Gregor MacLennan <gmaclennan@digital-democracy.org>
2015-03-29 16:28:38 -0700

    Cache formLists as buffers, up to 10Mb

--- controllers/github/get-formlist-github.js
+++ controllers/github/get-formlist-github.js
@@ -1,7 +1,17 @@
 var basicAuth = require('basic-auth')
 var createFormList = require('openrosa-formlist')
 var debug = require('debug')('simple-odk:get-formlist-github')
+var cacheManager = require('cache-manager')
+
 var getFormUrls = require('../../helpers/get-form-urls-github')
+
+// Cache up to 10Mb of formLists in memory, with a 5 min TTL
+var formListCache = cacheManager.caching({
+  store: 'memory',
+  max: 10 * 1000 * 1000,
+  ttl: 5 * 60, /*seconds*/
+  length: function (s) { return s.length }
+})
 
 /**
  * Searches for xml form files on Github and returns a valid
@@ -21,24 +31,34 @@ module.exports = function (req, res, next) {
     baseUrl: protocol + '://' + req.headers.host + req.baseUrl + '/forms'
   }
 
-  debug('Called formList for repo %s auth %s', options.user + '/' + options.repo)
+  var cacheKey = options.user + '/' + options.repo
 
-  getFormUrls(options, function (err, formUrls) {
+  debug('Called formList for repo %s', cacheKey)
+
+  formListCache.wrap(cacheKey, getFormListXml, function (err, buff) {
     if (err) return next(err)
-    debug('got form urls', formUrls)
-
-    var formlistOptions = {
-      headers: options.headers,
-      auth: {
-        user: options.auth.name,
-        pass: options.auth.pass
-      }
-    }
-
-    createFormList(formUrls, formlistOptions, function (err, formlistXml) {
-      if (err) return next(err)
-      res.set('content-type', 'text/xml; charset=utf-8')
-      res.status(200).send(formlistXml)
-    })
+    var formListXml = buff.toString()
+    res.set('content-type', 'text/xml; charset=utf-8')
+    res.status(200).send(formListXml)
   })
+
+  function getFormListXml (cb) {
+    getFormUrls(options, function (err, formUrls) {
+      if (err) return next(err)
+      debug('got form urls', formUrls)
+
+      var formlistOptions = {
+        headers: options.headers,
+        auth: {
+          user: options.auth.name,
+          pass: options.auth.pass
+        }
+      }
+
+      createFormList(formUrls, formlistOptions, function (err, xml) {
+        if (err) return cb(err)
+        cb(null, new Buffer(xml))
+      })
+    })
+  }
 }
