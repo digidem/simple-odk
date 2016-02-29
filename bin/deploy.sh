b792173e406408818c554abfa2bd707bdfefdd22
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-02-29 14:33:55 -0800

    Remove deployment specific files

--- bin/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-dt config:set VHOSTS="'$(cat vhost-config.json | tr -d '\n ')'" \
-ALIASES="'$(cat alias-config.json | tr -d '\n ')'" \
-$(cat .env.production | sed 's/\n/ /')
-git push collekt master
